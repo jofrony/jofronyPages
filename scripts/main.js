ce07df9a85ddea4fb091198c62e82b248903862e
jofrony <jofrony@gmail.com>
2020-12-24 02:06:49 +0100

    Switching to css, html js stuff

---
+++ scripts/main.js
@@ -0,0 +1,58 @@
+//const myHeading = document.querySelector('h1');
+//myHeading.textContent = 'Hello world!';
+
+let myVariable;
+myVariable = 'Bob';
+
+/*
+let iceCream = 'chocolate';
+if(iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('Awwww, but chocolate is my favorite...');
+}
+*/
+
+function multiply(num1,num2) {
+  let result = num1 * num2;
+  return result;
+}
+
+multiply(4, 7);
+multiply(20, 20);
+multiply(0.5, 3);
+
+document.querySelector('h1').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/graphical_abstract.png') {
+      myImage.setAttribute('src','images/under_construction.png');
+    } else {
+      myImage.setAttribute('src','images/graphical_abstract.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
